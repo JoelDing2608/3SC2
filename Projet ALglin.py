b7530f0d38f33b6d856b17adabc1ba083bfc4f6f
olivierbompas <37932193+olivierbompas@users.noreply.github.com>
2023-01-03 12:13:59 +0000

    gg

--- Projet ALglin.py
+++ Projet ALglin.py
@@ -1,37 +1,32 @@
 import numpy as np
-test_matx = np.array([[1,1,1],[1,2,3],[1,3,6]])
+import scipy as sc
+test_matx = np.array([[2,12,-3],[1,-6,-3],[2,0,18]])
 
-#décomposition QR
-def deco_QR(R):
-    n,m = np.shape(R)
+def deco_qr(A):
+    n = A.shape[0]
     Q = np.eye(n)
-    print(R)
-    for j in range (1,n-1):
-        print(j)
-        for i in range (j+1,n-1):
-            print(i)
-            r = np.sqrt(R[j,j]**2+R[i,j]**2)
-            if r != 0:
-                c = R[j,j]/r
-                s = R[i,j]/r
-                g = G(n,i,j,c,s)
-                print(g)
-                R = np.dot(g,R)
-                Q = Q*g.T
-                print (R)
-            else:
-                R = R
-                Q = Q
-    print(Q)
-    print (R)
+    
+    for j in range (n-1):
+        for i in range(n-1,j,-1):
+            lign1 = i
+            lign2 = i-1
+            r = np.sqrt(A[lign1,j]**2+A[lign2,j]**2)
+            g = G(n,lign1,lign2,A[lign2,j]/r,A[lign1,j]/r)
+            A = g*A
+            Q = Q*np.transpose(g)
 
+    m = n-1
+    if A[m,m] < 0:
+        A[m,m] = -A[m,m]
+        Q[:,2] = -Q[:,2]
+    print(Q)
+    print(A)
+    print(Q*A)
+
+    return (Q, A)
 def G(n,i,j,c,s):
     M = np.eye(n)
     M[i,i] = M[j,j] = c
     M[i,j],M[j,i] = -s, s
     return M
-deco_QR(test_matx)
-A = np.random.rand(n, n) #matrice aléatoire 
-    for i in range(0,n): 
-        for j in range (0,n): 
-            A[i,j]=randint(0,9)-4 
+deco_qr(test_matx)
--- test 2.py
+++ test 2.py
@@ -1,63 +1,26 @@
-# -*- coding: utf-8 -*-
-# python version 3.4 
- 
-import scipy as sc
-import numpy as np 
-from random import randint 
-"""factorisation QR méthode de Givens""" 
-###implémentation itérative 
- 
-def c(p,q,A): 
-    """cosinus theta""" 
-    if A[p,p]==0 and A[q,p]==0: 
-        return 1 
-    else: 
-        return A[p,p]/np.sqrt(A[p,p]*A[p,p]+A[q,p]*A[q,p]) 
- 
-def s(p,q,A): 
-    """sinus theta""" 
-    if A[p,p]==0 and A[q,p]==0: 
-        return 0 
-    else: 
-        return A[q,p]/np.sqrt(A[p,p]*A[p,p]+A[q,p]*A[q,p]) 
- 
-def G(q,p,A): 
-    """Selon l'algo""" 
-    G=np.eye(len(A)) #identité 
-    G[p,p]=c(p,q,A) 
-    G[p,q]=s(p,q,A) 
-    G[q,p]= -s(p,q,A) 
-    G[q,q]= c(p,q,A) 
-    return sc.matrix(G) 
- 
-def q(A): 
-    """calcule la transposée (donc l'inverse) du facteur Q""" 
-    n=len(A) 
-    q=np.eye(n) 
-    for i in range(0,n): 
-        for j in range(0,i): 
-            q=G(i,j,A)*q 
-            A=G(i,j,A)*A 
-    return q 
- 
-def main(): 
-    n=3 
-    A = np.array([[1,1,1],[1,2,3],[1,3,6]])
-    print (A) 
-    Q=q(A) 
-    R=Q*A 
-    print (R)
-    for i in range(0,n): 
-        for j in range (0,i): 
-            R[i,j]=0 #si résultat non exact 
+import numpy as np
+test_matx = np.array([[2,12,-3],[1,-6,-3],[2,0,18]])
+def qr_givens(A):
+    """
+    Décompose la matrice A en QR avec la méthode de Givens.
+    Retourne les matrices Q et R.
+    """
+    n, m = A.shape
+    Q = np.eye(n)
+    for j in range(m):
+        for i in range(n-1, j, -1):
+            G = np.eye(n)
+            r = np.hypot(A[i-1, j], A[i, j])
+            c = A[i-1, j] / r
+            s = A[i, j] / r
+            G[[i-1, i], [i-1, i]] = [c, s]
+            G[[i, i-1], [i-1, i]] = [-s, c]
+            A = G @ A
+            Q = Q @ G.T
+    R = A
+    print(Q)
     print(R)
- 
-'''    Q=Q.transpose() 
-    print ("Q=") 
-    print (Q) 
-    print ("R=") 
-    print (R) 
-    print (Q*R) '''
- 
-if __name__ == '__main__': 
-    main() 
+    print(Q@R)
+    return Q, R
+    
+qr_givens(test_matx)
