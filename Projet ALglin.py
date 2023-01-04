29ade1e4948d3f5cbe702dd12904d7039fd8f6c9
olivierbompas <37932193+olivierbompas@users.noreply.github.com>
2023-01-04 22:43:41 +0000

    comment

--- Projet ALglin.py
+++ Projet ALglin.py
@@ -16,50 +16,80 @@ def G(n,i,j,c,s):
 
 
 def qr_givens(A):
+    
+    # détermination de la taille de A 
     n, m = A.shape
+
     R = Matrix(A[:,:])
     Q = eye(n)
 
+    # boucle pour les colonnes de R augmentée
     for j in range (m-1):
+
+        #boucle pour les lignes de R augmentée
         for i in range(j+1,n):
 
             l1 = i
             l2 = i-1
             r = sqrt(R[l1,j]**2+R[l2,j]**2)
             g = G(n,l1,l2, R[l2,j]/r,R[l1,j]/r) 
+
+            #mise  à  jour de R
             R = g*R
+
+            #mise  à  jour de Q
             Q = g.T
             #pprint(R)
-    #pprint (Q)
-    #pprint(R)
     return (Q, R)
 
 
 def ResolGivens(A,b):
+
+    # décomposition QR de A
     Q,R = qr_givens(A)
+
+    #calcul de y
     y = Q.T*b
-    rt = R.inv()
+
+    #résolution de Rx = y
     #pprint(rt)
     f = linsolve((R,y))
     pprint(f)
     return f
 
+
 def ResolGivens2(A,b):
+
+    #création de la matrice augmentée
     A_aug = Matrix.hstack(A,b)
     #pprint(A_aug)
+
+    # détermination de la taille de A augmentée
     n, m = A_aug.shape
     R_aug = Matrix(A_aug[:,:])
+
+    # boucle pour les colonnes de R augmentée
     for j in range (m-1):
+
+        #boucle pour les lignes de R augmentée
         for i in range(j+1,n):
 
             l1 = i
             l2 = i-1
-            r = sqrt(R_aug[l1,j]**2+R_aug[l2,j]**2)
-            g = G(n,l1,l2, R_aug[l2,j]/r,R_aug[l1,j]/r) 
-            R_aug = g*R_aug
             
+            r = sqrt(R_aug[l1,j]**2+R_aug[l2,j]**2)
+
+            #calcul des coefficents et de la matrice G
+            g = G(n,l1,l2, R_aug[l2,j]/r,R_aug[l1,j]/r) 
+
+            #mise  à  jour de R augmentée
+            R_aug = g*R_aug
+
+    #extraction de b  dans R augmentée
     b_aug = R_aug[:,-1]
     #pprint(R_aug)
+
+    #résolution de Rx = b
     x = linsolve((R_aug[:,:-1],b_aug))
     pprint(x)
     return (x)
